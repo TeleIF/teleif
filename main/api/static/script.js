f14cc6a8a48abc5f72dc2b0b0393c7c56cc49f00
Daniel <danfeuser0@gmail.com>
2021-11-08 01:10:32 -0300

    removed ajax test from frontend

--- main/api/static/script.js
+++ main/api/static/script.js
@@ -3,13 +3,7 @@ document.querySelector('#msg-body').onkeyup = function(e) {
     if (e.keyCode === 13) {
         const messageInputDom = document.querySelector('#msg-body');
         const message = messageInputDom.value;
-        $.ajax({
-            type: 'POST',
-            url: 'post',
-            data: {
-                body: message,
-            },
-        });
+        
         messageInputDom.value = '';
     }
 }
