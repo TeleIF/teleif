477ef9fc05c4e42c1f042595b507c102e4f33070
danf0 <85913781+danf0@users.noreply.github.com>
2021-12-05 22:02:43 -0300

    Delete main directory

--- main/api/static/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#msg-body').focus();
-document.querySelector('#msg-body').onkeyup = function(e) {
-    if (e.keyCode === 13) {
-        const messageInputDom = document.querySelector('#msg-body');
-        const message = messageInputDom.value;
-        
-        messageInputDom.value = '';
-    }
-}
