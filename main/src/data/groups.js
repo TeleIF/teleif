2f7d72d8ce6b67ccf1e8083248681b62ab35562b
danf0 <danfeuser0@gmail.com>
2021-12-07 15:44:11 -0300

    update layout

--- main/src/data/groups.js
+++ main/src/data/groups.js
@@ -1,5 +1,6 @@
 export default [
     {id: 1, title: 'Group 1', lastMessage: {sender: 'Ana', time: '13:30', message: 'Bom dia'}},
     {id: 2, title: 'Group 2', lastMessage: {sender: 'Bia', time: '13:15', message: 'Do que eu estou falando é do whatsapp'}},
-    {id: 3, title: 'Group 3', lastMessage: {sender: 'Carlos', time: '13:10', message: 'rlx filhão controle parental e pro teu bem'}}
+    {id: 3, title: 'Group 3', lastMessage: {sender: 'Carlos', time: '13:10', message: 'rlx filhão controle parental e pro teu bem'}},
+    {id: 4, title: 'Group 4', lastMessage: {sender: 'Everson', time: '11:30', message: 'zoio'}}
 ]
