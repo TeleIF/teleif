d301ca072f8043fa68e3a2d8c6f6f22c10fac1da
danf0 <85913781+danf0@users.noreply.github.com>
2022-03-02 17:50:06 -0300

    Update firebase-config.js

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -4,14 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBhV5bhggGk5shwwBpRtkmvGU4IM17f9nQ",
-    authDomain: "teleif-4f42f.firebaseapp.com",
-    databaseURL: "https://teleif-4f42f-default-rtdb.firebaseio.com",
-    projectId: "teleif-4f42f",
-    storageBucket: "teleif-4f42f.appspot.com",
-    messagingSenderId: "913167733686",
-    appId: "1:913167733686:web:4292101741aca8fd306717",
-    measurementId: "G-JXDG9J3RG0",
+    // Here's where the firebase settings go
 };
 
 const app = initializeApp(firebaseConfig);
